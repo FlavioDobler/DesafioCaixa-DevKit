2ef09f31a2050467dd2f392a8c1a85f96db3e198
Flavio Dobler <flaviodobler@Flavios-MacBook-Air.local>
2023-04-20 00:13:13 -0300

    Adicionado quantidades de notas possiveis

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,6 +5,11 @@ enum Cedulas: Int {
     case dois = 2
 }
 
+var estoqueCinquenta = 3
+var estoqueDez = 3
+var estoqueCinco = 3
+var estoqueDois = 3
+
 var count50 = 0
 var count10 = 0
 var count5 = 0
@@ -14,23 +19,33 @@ func giveMoneyPls(_ valor: Int) -> String {
     var amount = valor
     
     while amount > 0 {
-        if amount >= Cedulas.cinquenta.rawValue {
+        
+        if amount >= Cedulas.cinquenta.rawValue && estoqueCinquenta > 0 {
             count50 += 1
             amount -= Cedulas.cinquenta.rawValue
-        } else if amount >= Cedulas.dez.rawValue {
+            estoqueCinquenta -= 1
+        } else if amount >= Cedulas.dez.rawValue && estoqueDez > 0{
             count10 += 1
             amount -= Cedulas.dez.rawValue
-        } else if amount >= Cedulas.cinco.rawValue && amount % 5 == 0 {
+            estoqueDez -= 1
+        } else if amount >= Cedulas.cinco.rawValue && amount % 5 == 0 && estoqueCinco > 0{
             count5 += 1
             amount -= Cedulas.cinco.rawValue
-        } else {
+            estoqueCinco -= 1
+        } else if amount >= Cedulas.dois.rawValue && estoqueDois > 0 {
             count2 += 1
             amount -= Cedulas.dois.rawValue
+            estoqueDois -= 1
+        } else if estoqueCinquenta > 0 || estoqueDez > 0 || estoqueCinco > 0 || estoqueDois > 0 {
+            return "O Valor de notas de Cinquenta é \(count50), de Dez é \(count10), de Cinco é \(count5), e de Dois é \(count2)."
+        } else {
+            return("Valor indisponivel para saque")
         }
     }
+
+    return ""
     
-    return "O Valor de notas de Cinquenta é \(count50), de Dez é \(count10), de Cinco é \(count5), e de Dois é \(count2)."
 }
 
-var dinheiros = giveMoneyPls(40)
+var dinheiros = giveMoneyPls(200)
 print(dinheiros)
